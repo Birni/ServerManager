bc1fda09a91d95a1597af68c7315f6f53427a231
Birni <Matthias-Birnthaler@hotmail.de>
2018-10-03 13:25:11 +0200

    first implementation of discord bot

---
+++ DiscordBot/Commands/Troll.cs
@@ -0,0 +1,40 @@
+﻿using DiscordBot.Entities;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    [Group("troll"), Aliases("asdf")]
+    internal class Lol
+    {
+        Dependencies dep;
+        public Lol(Dependencies d)
+        {
+            this.dep = d;
+        }
+
+        [Command("lol")]
+        public async Task ConfirmationAsync(CommandContext ctx)
+        {
+           await ctx.RespondAsync("▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▒");
+            await ctx.RespondAsync("▒▒▒▒▒▒▄▀▀▓▓▓▀█▒▒▒▒▒▒");
+            await ctx.RespondAsync("▒▒▒▒▄▀▓▓▄██████▄▒▒▒▒");
+            await ctx.RespondAsync("▒▒▒▄█▄█▀░░▄░▄░█▀▒▒▒▒");
+            await ctx.RespondAsync("▒▒▄▀░██▄░░▀░▀░▀▄▒▒▒▒");
+            await ctx.RespondAsync("▒▒▀▄░░▀░▄█▄▄░░▄█▄▒▒▒");
+            await ctx.RespondAsync("▒▒▒▒▀█▄▄░░▀▀▀█▀▒▒▒▒▒");
+            await ctx.RespondAsync("▒▒▒▄▀▓▓▓▀██▀▀█▄▀▀▄▒▒");
+            await ctx.RespondAsync("▒▒█▓▓▄▀▀▀▄█▄▓▓▀█░█▒▒");
+            await ctx.RespondAsync("▒▒▀▄█░░░░░█▀▀▄▄▀█▒▒▒");
+            await ctx.RespondAsync("▒▒▒▄▀▀▄▄▄██▄▄█▀▓▓█▒▒");
+            await ctx.RespondAsync("▒▒█▀▓█████████▓▓▓█▒▒");
+            await ctx.RespondAsync("▒▒█▓▓██▀▀▀▒▒▒▀▄▄█▀▒▒");
+            await ctx.RespondAsync("▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+        }
+    }
+}
